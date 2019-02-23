9ff9c9aec7ccdbec2bb6295b18339a67f67c0f98
devJWSong <enflwodn@gmail.com>
2019-02-23 17:06:51 +0900

    tip/event edit api added

--- routes/api/event.js
+++ routes/api/event.js
@@ -43,7 +43,7 @@ function decodeToken(token) {
     return promise;
 }
 
-async function putImage(file, folderName, fileName) {
+async function putImage(res, file, folderName, fileName) {
     const params = {
         Bucket: config.s3Bucket,
         Key: null,
@@ -76,13 +76,33 @@ async function putImage(file, folderName, fileName) {
     return params.Key;
 }
 
+// function to delete an image in s3 bucket
+async function deleteImage(res, folderName, fileName, fileUrl) {
+    const params = {
+        Bucket: config.s3Bucket,
+        Key: null,
+    };
+
+    params.Key = folderName + fileName + getExtension(fileUrl);
+
+    await s3.deleteObject(params, (err, data) => {
+        if (err){
+            res.status(424).json({
+                error: "s3 delete failed"
+            });
+            return;
+        }
+    });
+
+    return params.Key;
+}
 
 /*
     > admin이 이벤트를 작성하는 api
     > POST /api/event/post
     > form data로 데이터 전달. 각 데이터의 이름은 디비와 통일.
     > 필수정보: title(포스트 제목),subtitle(포스트 부제목), content(포스트 내용), expirationDate(만료 날짜), titleImage(표지 이미지), contentImage(내용 이미지)
-      content는 없을 경우 빈 string "" 보낼 것
+      content는 없을 경우 빈 string "" 보낼 것. exirationDate의 형식은 "YYYY MM DD HH:MM:SS"(ex> 2019-02-20 00:00:00)
     > error: {
           "invalid request": 올바른 req가 전달되지 않음
           "unauthorized request": 사용 권한이 없는 접근
@@ -115,7 +135,7 @@ router.post('/post', formidable(), (req, res) => {
         }
 
         if (!req.fields.title || !req.fields.subtitle || !req.fields.content || !req.files.titleImage || typeof req.files.titleImage === 'undefined' 
-                || !req.files.contentImage || typeof req.files.contentImage === 'undefined') {
+                || !req.files.contentImage || typeof req.files.contentImage === 'undefined' || !req.fields.expirationDate) {
             res.status(400).json({
                 error: "invalid request"
             });
@@ -168,6 +188,118 @@ router.post('/post', formidable(), (req, res) => {
             });
         });
 
+    }).catch((error) => {
+        res.status(403).json({
+            error: "unauthorized request"
+        });
+        return;
+    });
+});
+
+/*
+    > admin이 이벤트를 수정하는 api
+    > PUT /api/event/post?index=1
+    > form data로 데이터 전달. 각 데이터의 이름은 디비와 통일.
+    > 필수정보: title(포스트 제목),subtitle(포스트 부제목), content(포스트 내용), expirationDate(만료 일자) titleImage(표지 사진), contentImage(내용 사진) 
+      content는 없을 경우 빈 string "" 보낼 것, exirationDate의 형식은 "YYYY MM DD HH:MM:SS"(ex> 2019-02-20 00:00:00),
+      해당하는 포스트의 index를 req.query.index로 전달
+    > error: {
+          "invalid request": 올바른 req가 전달되지 않음
+          "unauthorized request": 사용 권한이 없는 접근
+          "invalied file(image only)": 이미지가 아닌 파일이 넘어옴
+          "no such post": 해당 포스트는 존재하지 않음
+          "s3 delete failed": s3 버켓 안의 이미지 삭제 실해
+          "s3 store failed": s3 버켓에 이미지 저장 실패
+          "post update failed": 수정 실패
+      }
+    > success: {
+        true: 성공적으로 변경
+      }
+*/
+router.put('/post', formidable(), (req, res) => {
+    let token = req.headers['authorization'];  
+
+    postObj = {};
+
+    decodeToken(token).then((token) => {
+        if (!token.index || !token.email || !token.nickName) {
+            res.status(400).json({
+                error: "invalid request"
+            });
+            return;
+        }
+
+        if (token.index !== 1) {
+            res.status(403).json({
+                error: "unauthorized request"
+            });
+            return;
+        }
+
+        if (!req.fields.title || !req.fields.subtitle || !req.fields.content || !req.files.titleImage || typeof req.files.titleImage === 'undefined' 
+                || !req.files.contentImage || typeof req.files.contentImage === 'undefined' || !req.fields.expirationDate) {
+            res.status(400).json({
+                error: "invalid request"
+            });
+            return;
+        }
+
+        if (!moment(req.fields.expirationDate).isValid() || !moment(req.fields.expirationDate).isAfter(moment())) {
+            res.status(400).json({
+                error: "invalid request"
+            });
+            return;
+        }
+
+        postObj.title = req.fields.title;
+        postObj.subtitle = req.fields.subtitle;
+        postObj.content = req.fields.content;
+        postObj.expirationDate = moment(req.fields.expirationDate);
+
+        db.Event.findOne({
+            where: {
+                index: Number(req.query.index)
+            }
+        }).then((result) => {
+            if (!result) {
+                res.status(424).json({
+                    error: "no such post"
+                });
+                return;
+            } else {
+                deleteImage(res, 'event-images/title-images/', result.dataValues.index.toString(), result.dataValues.titleImageUrl).then(key => {
+                    deleteImage(res, 'event-images/content-images/', result.dataValues.index.toString(), result.dataValues.contentImageUrl).then(key => {
+                        putImage(res, req.files.titleImage, 'event-images/title-images/', result.dataValues.index.toString()).then(key => {
+                            postObj.titleImageUrl = config.s3Url + key;
+
+                            putImage(res, req.files.contentImage, 'event-images/content-images/', result.dataValues.index.toString()).then(key => {
+                                postObj.contentImageUrl = config.s3Url + key;
+
+                                db.Event.update(
+                                    postObj,
+                                    {
+                                        where: {
+                                            index: result.dataValues.index
+                                        }
+                                    }
+                                ).then((result) => {
+                                    if (!result) {
+                                        res.status(424).json({
+                                            error: "post update failed"
+                                        });
+                                        return;
+                                    } else {
+                                        res.json({
+                                            success: true
+                                        });
+                                    }
+                                });
+                            });
+                        });
+                    });
+                });
+            }
+        });
     }).catch((error) => {
         res.status(403).json({
             error: "unauthorized request"
--- routes/api/tip.js
+++ routes/api/tip.js
@@ -43,7 +43,8 @@ function decodeToken(token) {
     return promise;
 }
 
-async function putImage(file, folderName, fileName) {
+// function to put an image into s3 bucket
+async function putImage(res, file, folderName, newName) {
     const params = {
         Bucket: config.s3Bucket,
         Key: null,
@@ -60,7 +61,7 @@ async function putImage(file, folderName, fileName) {
         });
         return;
     } else {
-        params.Key = folderName + fileName + getExtension(file.name);
+        params.Key = folderName + newName + getExtension(file.name);
         params.Body = require('fs').createReadStream(file.path);
     }
 
@@ -72,6 +73,27 @@ async function putImage(file, folderName, fileName) {
             return;
         }
     })
+
+    return params.Key;
+}
+
+// function to delete an image in s3 bucket
+async function deleteImage(res, folderName, fileName, fileUrl) {
+    const params = {
+        Bucket: config.s3Bucket,
+        Key: null,
+    };
+
+    params.Key = folderName + fileName + getExtension(fileUrl);
+
+    await s3.deleteObject(params, (err, data) => {
+        if (err){
+            res.status(424).json({
+                error: "s3 delete failed"
+            });
+            return;
+        }
+    });
 
     return params.Key;
 }
@@ -138,10 +160,10 @@ router.post('/post', formidable(), (req, res) => {
                 nextIndex = result[0].dataValues.index + 1;
             }
 
-            putImage(req.files.titleImage, 'tip-images/title-images/', nextIndex).then(key => {
+            putImage(res, req.files.titleImage, 'tip-images/title-images/', nextIndex.toString()).then(key => {
                 postObj.titleImageUrl = config.s3Url + key;
 
-                putImage(req.files.contentImage, 'tip-images/content-images/', nextIndex).then(key => {
+                putImage(res, req.files.contentImage, 'tip-images/content-images/', nextIndex.toString()).then(key => {
                     postObj.contentImageUrl = config.s3Url + key;
                     
                     db.HoneyTip.create(
@@ -169,11 +191,11 @@ router.post('/post', formidable(), (req, res) => {
 });
 
 /*
-    > admin이 꿀팁/이벤트를 수정하는 api
-    > PUT /api/tipEvent/post?index=1?isTip=true
+    > admin이 꿀팁을 수정하는 api
+    > PUT /api/tip/post?index=1
     > form data로 데이터 전달. 각 데이터의 이름은 디비와 통일.
-    > 필수정보: title(포스트 제목),subtitle(포스트 부제목), content(포스트 내용), expirationDate(만료 일자), 
-      content는 없을 경우 빈 string "" 보낼 것, req.query.isTip으로 팁인지 이벤트인지 여부 전달.(팁이면 true, 이벤트면 false), 해당하는 포스트의 index를 req.query.index로 전달
+    > 필수정보: title(포스트 제목),subtitle(포스트 부제목), content(포스트 내용), titleImage(표지 사진), contentImage(내용 사진) 
+      content는 없을 경우 빈 string "" 보낼 것, 해당하는 포스트의 index를 req.query.index로 전달
     > error: {
           "invalid request": 올바른 req가 전달되지 않음
           "unauthorized request": 사용 권한이 없는 접근
@@ -188,19 +210,7 @@ router.post('/post', formidable(), (req, res) => {
       }
 */
 router.put('/post', formidable(), (req, res) => {
-    let token = req.headers['authorization'];
-
-    const addParams = {
-        Bucket: config.s3Bucket,
-        Key: null,
-        ACL: 'public-read',
-        Body: null
-    };
-
-    const deleteParams = {
-        Bucket: config.s3Bucket,
-        Key: null,
-    };  
+    let token = req.headers['authorization'];  
 
     postObj = {};
 
@@ -219,22 +229,8 @@ router.put('/post', formidable(), (req, res) => {
             return;
         }
 
-        if (!req.fields.title || !req.fields.subtitle || !req.fields.content || !req.fields.expirationDate || !req.files.postImage
-                || typeof req.files.postImage === 'undefined' || !req.query.isTip) {
-            res.status(400).json({
-                error: "invalid request"
-            });
-            return;
-        }
-
-        if (req.query.isTip !== 'true' && req.query.isTip !== 'false') {
-            res.status(400).json({
-                error: "invalid request"
-            });
-            return;
-        }
-
-        if (!moment(req.fields.expirationDate).isValid() || !moment(req.fields.expirationDate).isAfter(moment())) {
+        if (!req.fields.title || !req.fields.subtitle || !req.fields.content || !req.files.titleImage || typeof req.files.titleImage === 'undefined' 
+        || !req.files.contentImage || typeof req.files.contentImage === 'undefined' || !req.query.index) {
             res.status(400).json({
                 error: "invalid request"
             });
@@ -244,133 +240,51 @@ router.put('/post', formidable(), (req, res) => {
         postObj.title = req.fields.title;
         postObj.subtitle = req.fields.subtitle;
         postObj.content = req.fields.content;
-        postObj.expirationDate = moment(req.fields.expirationDate);
 
-        if (!(req.files.postImage.type ===  'image/gif' 
-                || req.files.postImage.type === 'image/jpg' 
-                || req.files.postImage.type === 'image/png'
-                || req.files.postImage.type === 'image/jpeg')) {
-            res.status(400).json({
-                error: "invalid file(only image)"
-            });
-        }
+        db.HoneyTip.findOne({
+            where: {
+                index: Number(req.query.index)
+            }
+        }).then((result) => {
+            if (!result) {
+                res.status(424).json({
+                    error: "no such post"
+                });
+                return;
+            } else {
+                deleteImage(res, 'tip-images/title-images/', result.dataValues.index.toString(), result.dataValues.titleImageUrl).then(key => {
+                    deleteImage(res, 'tip-images/content-images/', result.dataValues.index.toString(), result.dataValues.contentImageUrl).then(key => {
+                        putImage(res, req.files.titleImage, 'tip-images/title-images/', result.dataValues.index.toString()).then(key => {
+                            postObj.titleImageUrl = config.s3Url + key;
 
-        if (req.query.isTip === 'true') {
-            db.HoneyTip.findOne({
-                where: {
-                    index: Number(req.query.index),
-                }
-            }).then((result) => {
-                if (!result) {
-                    req.status(424).json({
-                        error: "no such post"
-                    });
-                    return;
-                } else {
-                    deleteParams.Key = "tip-images/" + result.dataValues.index.toString() + getExtension(result.dataValues.photoUrl);
-                    s3.deleteObject(deleteParams, (err, data) => {
-                        if (err) {
-                            req.status(424).json({
-                                error: "s3 delete failed"
-                            });
-                            return;
-                        } else {
-                            addParams.Key = "tip-images/" + result.dataValues.index.toString() + getExtension(req.files.postImage.name);
-                            addParams.Body = require('fs').createReadStream(req.files.postImage.path);
-                            postObj.photoUrl = config.s3Url + addParams.Key;
+                            putImage(res, req.files.contentImage, 'tip-images/content-images/', result.dataValues.index.toString()).then(key => {
+                                postObj.contentImageUrl = config.s3Url + key;
 
-                            s3.putObject(addParams, (err, data) => {
-                                if (err) {
-                                    req.status(424).json({
-                                        error: "s3 add failed"
-                                    });
-                                    return;
-                                } else {
-                                    db.HoneyTip.update(
-                                        postObj,
-                                        {
-                                            where: {
-                                                index: result.dataValues.index
-                                            }
+                                db.HoneyTip.update(
+                                    postObj,
+                                    {
+                                        where: {
+                                            index: result.dataValues.index
                                         }
-                                    ).then((result) => {
-                                        if (!result) {
-                                            req.status(424).json({
-                                                error: "post update failed"
-                                            });
-                                            return;
-                                        } else {
-                                            res.json({
-                                                success: true
-                                            });
-                                            return;
-                                        }
-                                    });
-                                }
+                                    }
+                                ).then((result) => {
+                                    if (!result) {
+                                        res.status(424).json({
+                                            error: "post update failed"
+                                        });
+                                        return;
+                                    } else {
+                                        res.json({
+                                            success: true
+                                        });
+                                    }
+                                });
                             });
-                        }
+                        });
                     });
-                }
-            });
-        } else {
-            db.Event.findOne({
-                where: {
-                    index: Number(req.query.index),
-                }
-            }).then((result) => {
-                if (!result) {
-                    req.status(424).json({
-                        error: "no such post"
-                    });
-                    return;
-                } else {
-                    deleteParams.Key = "event-images/" + result.dataValues.index.toString() + getExtension(result.dataValues.photoUrl);
-                    s3.deleteObject(deleteParams, (err, data) => {
-                        if (err) {
-                            req.status(424).json({
-                                error: "s3 delete failed"
-                            });
-                            return;
-                        } else {
-                            addParams.Key = "event-images/" + result.dataValues.index.toString() + getExtension(req.files.postImage.name);
-                            addParams.Body = require('fs').createReadStream(req.files.postImage.path);
-                            postObj.photoUrl = config.s3Url + addParams.Key;
-
-                            s3.putObject(addParams, (err, data) => {
-                                if (err) {
-                                    req.status(424).json({
-                                        error: "s3 add failed"
-                                    });
-                                    return;
-                                } else {
-                                    db.Event.update(
-                                        postObj,
-                                        {
-                                            where: {
-                                                index: result.dataValues.index
-                                            }
-                                        }
-                                    ).then((result) => {
-                                        if (!result) {
-                                            req.status(424).json({
-                                                error: "post update failed"
-                                            });
-                                            return;
-                                        } else {
-                                            res.json({
-                                                success: true
-                                            });
-                                            return;
-                                        }
-                                    });
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        }
-
+                });
+            }
+        });
     }).catch((error) => {
         res.status(403).json({
             error: "unauthorized request"
